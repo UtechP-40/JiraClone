d010cc69520aa36b0aa8591f46a53b5e3cee9a9c
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-09-15 21:57:40 +0530

    Landing Page Completed

---
+++ script.js
@@ -0,0 +1,8 @@
+let singIn = document.getElementById("btn1");
+let Register = document.getElementById("btn2");
+singIn.addEventListener("click",(e)=>{
+    window.location.href = "/pages/SignIn.html"
+})
+Register.addEventListener("click",(e)=>{
+    window.location.href = "/pages/SignIn.html"
+})
