9ee392a0273b6058fbb2e341c76abbbd9e233876
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-09-16 15:23:21 +0530

    Pages Fixed:2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let singIn = document.getElementById("btn1");
 let Register = document.getElementById("btn2");
 singIn.addEventListener("click",(e)=>{
-    window.location.href = "/Pages/SignIn.html"
+    window.location.href = "./Pages/SignIn.html"
 })
 Register.addEventListener("click",(e)=>{
-    window.location.href = "/Pages/SignIn.html"
+    window.location.href = "./Pages/SignIn.html"
 })
